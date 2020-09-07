555dee59054066d7e31523dc272bbe14bd33a76d
zzzonked <yaroslavkuts@gmail.com>
2020-09-07 14:13:17 +0300

    inject table name through di container

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,23 +1,31 @@
-import { Injectable, Module, OnModuleInit } from '@nestjs/common';
+import { Inject, Injectable, Module, OnModuleInit } from '@nestjs/common';
 import * as AWS from 'aws-sdk';
 
-export const TABLE_NAME = 'Main';
+const TABLE_NAME_VALUE = 'Main';
+export const TABLE_NAME_TOKEN = 'table-name-token';
 
 @Injectable()
 @Module({
-  providers: [AWS.DynamoDB, AWS.DynamoDB.DocumentClient],
-  exports: [AWS.DynamoDB, AWS.DynamoDB.DocumentClient]
+  providers: [
+    AWS.DynamoDB,
+    AWS.DynamoDB.DocumentClient,
+    { provide: TABLE_NAME_TOKEN, useValue: TABLE_NAME_VALUE },
+  ],
+  exports: [AWS.DynamoDB, AWS.DynamoDB.DocumentClient, TABLE_NAME_TOKEN]
 })
 export class DatabaseModule implements OnModuleInit {
-  constructor(private readonly db: AWS.DynamoDB) {}
+  constructor(
+    @Inject(TABLE_NAME_TOKEN) private readonly table: string,
+    private readonly db: AWS.DynamoDB,
+  ) {}
 
 
   async onModuleInit(): Promise<void> {
     const result = await this.db.listTables().promise();
-    if (result.TableNames.includes(TABLE_NAME)) return;
+    if (result.TableNames.includes(this.table)) return;
 
     await this.db.createTable({
-      TableName: TABLE_NAME,
+      TableName: this.table,
       KeySchema: [{ AttributeName: 'id', KeyType: 'HASH' }],
       AttributeDefinitions: [{ AttributeName: 'id', AttributeType: 'S' }],
       ProvisionedThroughput: {
--- src/users/users.repository.ts
+++ src/users/users.repository.ts
@@ -1,28 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import * as AWS from 'aws-sdk';
 import { v4 as uuid } from 'uuid';
 
 import { UserDto, UserInputDto } from './dto';
 
-import { TABLE_NAME } from '../database/database.module';
+import { TABLE_NAME_TOKEN } from '../database/database.module';
 
 @Injectable()
 export class UsersRepository {
-  constructor(private readonly client: AWS.DynamoDB.DocumentClient) {}
+  constructor(
+    @Inject(TABLE_NAME_TOKEN) private readonly tableName: string,
+    private readonly client: AWS.DynamoDB.DocumentClient,
+  ) {}
 
   async getAll(): Promise<Array<UserDto>> {
-    const result = await this.client.scan({ TableName: TABLE_NAME }).promise();
+    const result = await this.client.scan({ TableName: this.tableName }).promise();
     return (result.Items as Array<UserDto>);
   }
 
   async get(id: string): Promise<UserDto> {
-    const result = await this.client.get({ TableName: TABLE_NAME, Key: { id } }).promise();
+    const result = await this.client.get({ TableName: this.tableName, Key: { id } }).promise();
     return (result.Item as UserDto);
   }
 
   async create(user: UserInputDto): Promise<UserDto> {
     const item = { id: uuid(), ...user }
-    await this.client.put({ TableName: TABLE_NAME, Item: item }).promise();
+    await this.client.put({ TableName: this.tableName, Item: item }).promise();
     return item;
   }
 
@@ -32,7 +35,7 @@ export class UsersRepository {
 
     await this.client
       .update({
-        TableName: TABLE_NAME,
+        TableName: this.tableName,
         Key: { id },
         UpdateExpression: 'set #name = :n, #email =:e',
         ExpressionAttributeNames: { '#name': 'name', '#email': 'email' },
@@ -45,7 +48,7 @@ export class UsersRepository {
 
   async delete(id: string): Promise<UserDto> {
     const user = await this.get(id);
-    if (user) await this.client.delete({ TableName: TABLE_NAME, Key: { id } }).promise();
+    if (user) await this.client.delete({ TableName: this.tableName, Key: { id } }).promise();
     return user;
   }
 }
