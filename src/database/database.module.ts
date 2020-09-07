972076309004031bd785fb394982b339ebe2b644
zzzonked <yaroslavkuts@gmail.com>
2020-09-07 13:59:43 +0300

    move table creation to the database module

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,9 +1,30 @@
-import { Module, Global } from '@nestjs/common';
+import { Injectable, Module, Global, OnModuleInit } from '@nestjs/common';
 import * as AWS from 'aws-sdk';
 
+export const TABLE_NAME = 'Main';
+
+@Injectable()
 @Global()
 @Module({
   providers: [AWS.DynamoDB, AWS.DynamoDB.DocumentClient],
   exports: [AWS.DynamoDB, AWS.DynamoDB.DocumentClient]
 })
-export class DatabaseModule {}
+export class DatabaseModule implements OnModuleInit {
+  constructor(private readonly db: AWS.DynamoDB) {}
+
+
+  async onModuleInit(): Promise<void> {
+    const result = await this.db.listTables().promise();
+    if (result.TableNames.includes(TABLE_NAME)) return;
+
+    await this.db.createTable({
+      TableName: TABLE_NAME,
+      KeySchema: [{ AttributeName: 'id', KeyType: "HASH" }],
+      AttributeDefinitions: [{ AttributeName: 'id', AttributeType: 'S' }],
+      ProvisionedThroughput: {
+        ReadCapacityUnits: 10,
+        WriteCapacityUnits: 10
+      },
+    }).promise();
+  }
+}
--- src/users/users.repository.ts
+++ src/users/users.repository.ts
@@ -1,47 +1,28 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import * as AWS from 'aws-sdk';
 import { v4 as uuid } from 'uuid';
 
 import { UserDto, UserInputDto } from './dto';
 
+import { TABLE_NAME } from '../database/database.module';
+
 @Injectable()
-export class UsersRepository implements OnModuleInit {
-  private readonly table = 'users';
-
-  constructor(
-    private readonly db: AWS.DynamoDB,
-    private readonly client: AWS.DynamoDB.DocumentClient,
-  ) {}
-
-
-  async onModuleInit(): Promise<void> {
-    const result = await this.db.listTables().promise();
-    if (result.TableNames.includes(this.table)) return;
-
-    await this.db.createTable({
-      TableName : this.table,
-      KeySchema: [{ AttributeName: 'id', KeyType: "HASH" }],
-      AttributeDefinitions: [{ AttributeName: 'id', AttributeType: 'S' }],
-      ProvisionedThroughput: {
-        ReadCapacityUnits: 10,
-        WriteCapacityUnits: 10
-      },
-    }).promise();
-  }
+export class UsersRepository {
+  constructor(private readonly client: AWS.DynamoDB.DocumentClient) {}
 
   async getAll(): Promise<Array<UserDto>> {
-    const result = await this.client.scan({ TableName: this.table }).promise();
+    const result = await this.client.scan({ TableName: TABLE_NAME }).promise();
     return (result.Items as Array<UserDto>);
   }
 
   async get(id: string): Promise<UserDto> {
-    const result = await this.client.get({ TableName: this.table, Key: { id } }).promise();
+    const result = await this.client.get({ TableName: TABLE_NAME, Key: { id } }).promise();
     return (result.Item as UserDto);
   }
 
   async create(user: UserInputDto): Promise<UserDto> {
     const item = { id: uuid(), ...user }
-    await this.client.put({ TableName: this.table, Item: item }).promise();
+    await this.client.put({ TableName: TABLE_NAME, Item: item }).promise();
     return item;
   }
 
@@ -51,7 +32,7 @@ export class UsersRepository implements OnModuleInit {
 
     await this.client
       .update({
-        TableName: this.table,
+        TableName: TABLE_NAME,
         Key: { id },
         UpdateExpression: 'set #name = :n, #email =:e',
         ExpressionAttributeNames: { '#name': 'name', '#email': 'email' },
@@ -64,7 +45,7 @@ export class UsersRepository implements OnModuleInit {
 
   async delete(id: string): Promise<UserDto> {
     const user = await this.get(id);
-    if (user) await this.client.delete({ TableName: this.table, Key: { id } }).promise();
+    if (user) await this.client.delete({ TableName: TABLE_NAME, Key: { id } }).promise();
     return user;
   }
 }
